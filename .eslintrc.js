7fb321d87f79685caee5f269fecae61dda3924f7
Egor Dorosh <egor.dorosh@libertexgroup.com>
2019-08-06 18:05:37 +0200

    Added paralax

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,10 @@ module.exports = {
     "extends": "standard",
     "rules": {
       "semi": ["error", "always"],
+      "at-rule-no-unknown": [
+      "true",{
+        "ignoreAtRules": ["/^b/i"]
+      }
+    ]
     }
 }
